37522983faa0355958e1f5091c585947b6ab43b5
Tayla van Ingen <60573737+TaylavanIngen@users.noreply.github.com>
2020-04-05 10:27:12 +0200

    initial commit

---
+++ Dicee Challenge - Starting Files/script.js
@@ -0,0 +1,46 @@
+//RANDOM NUMBER
+
+
+const createRandomDice1=function(){
+
+let randomNumber1= Math.floor((Math.random()*6)+1);
+
+document.querySelector(".img1").src=`images/dice${randomNumber1}.png`;
+
+return randomNumber1;
+
+}
+
+const createRandomDice2=function(){
+
+let randomNumber2= Math.floor((Math.random()*6)+1);
+
+document.querySelector(".img2").src=`images/dice${randomNumber2}.png`;
+
+return randomNumber2;
+
+}
+
+
+const whoWins=function(){
+let randomDice1=createRandomDice1();
+let randomDice2=createRandomDice2();
+
+if (randomDice1>randomDice2){
+
+document.querySelector("h1").innerHTML="Player 1 wins!";
+
+}
+
+else if (randomDice1< randomDice2){
+
+  document.querySelector("h1").innerHTML="Player 2 wins!";
+}
+
+else if (randomDice1===randomDice2){
+  document.querySelector("h1").innerHTML="Draw";
+}
+
+}
+
+whoWins();
