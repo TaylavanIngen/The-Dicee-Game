363bbf30ed0afac9647a74d5e4a0276e539ea736
Tayla van Ingen <60573737+TaylavanIngen@users.noreply.github.com>
2020-04-05 10:34:17 +0200

    update beautified

--- Dicee Challenge - Starting Files/script.js
+++ Dicee Challenge - Starting Files/script.js
@@ -1,46 +1,39 @@
 //RANDOM NUMBER
 
 
-const createRandomDice1=function(){
+const createRandomDice1 = function() {
 
-let randomNumber1= Math.floor((Math.random()*6)+1);
+  let randomNumber1 = Math.floor((Math.random() * 6) + 1);
 
-document.querySelector(".img1").src=`images/dice${randomNumber1}.png`;
+  document.querySelector(".img1").src = `images/dice${randomNumber1}.png`;
 
-return randomNumber1;
-
-}
-
-const createRandomDice2=function(){
-
-let randomNumber2= Math.floor((Math.random()*6)+1);
-
-document.querySelector(".img2").src=`images/dice${randomNumber2}.png`;
-
-return randomNumber2;
+  return randomNumber1;
 
 }
 
+const createRandomDice2 = function() {
 
-const whoWins=function(){
-let randomDice1=createRandomDice1();
-let randomDice2=createRandomDice2();
+  let randomNumber2 = Math.floor((Math.random() * 6) + 1);
 
-if (randomDice1>randomDice2){
+  document.querySelector(".img2").src = `images/dice${randomNumber2}.png`;
 
-document.querySelector("h1").innerHTML="Player 1 wins!";
-
-}
-
-else if (randomDice1< randomDice2){
-
-  document.querySelector("h1").innerHTML="Player 2 wins!";
-}
-
-else if (randomDice1===randomDice2){
-  document.querySelector("h1").innerHTML="Draw";
-}
+  return randomNumber2;
 
 }
 
-whoWins();
+
+//WHO WINS?
+
+let randomDice1 = createRandomDice1();
+let randomDice2 = createRandomDice2();
+
+if (randomDice1 > randomDice2) {
+
+  document.querySelector("h1").innerHTML = "Player 1 wins!";
+
+} else if (randomDice1 < randomDice2) {
+
+  document.querySelector("h1").innerHTML = "Player 2 wins!";
+} else if (randomDice1 === randomDice2) {
+  document.querySelector("h1").innerHTML = "Draw";
+}
